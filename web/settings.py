45f5020386aa9381b736f5e46a518ecc4022e063
saleekah ck <you@example.com>
2020-09-10 08:25:59 +0530

    com5

--- web/settings.py
+++ web/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = '6z=o3^#%ckyzwcje3zdcbwxl479nky+!+dtb60x9mg*jrtjb!^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hygio.herokuapp.com' ]
 
 
 
